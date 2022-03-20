b8961f9804a17993dd2acb58b53339c9af836777
JasonNong <98986254+JasonNong@users.noreply.github.com>
2022-03-20 18:50:53 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+function description1() {
+  document.getElementById("1").innerHTML = "sympathetic!";
+}
+
+function word1() {
+  document.getElementById("1").innerHTML = "Commiserating";
+}
+
+
+
+function description2() {
+  document.getElementById("2").innerHTML = "(: charitable";
+}
+
+function word2() {
+  document.getElementById("2").innerHTML = "Eleemosynary";
+}
+
+  
+function description3() {
+  document.getElementById("3").innerHTML = "perceptive!";
+}
+
+function word3() {
+  document.getElementById("3").innerHTML = "Scrupulous";
+}
+
+
+
+function description4() {
+  document.getElementById("4").innerHTML = "compatible :)";
+}
+
+function word4() {
+  document.getElementById("4").innerHTML = "Congenial";
+}
+
+function description5() {
+  document.getElementById("5").innerHTML = "calm (really??)";
+}
+
+function word5() {
+  document.getElementById("5").innerHTML = "Imperturbable";
+}
+
+function description6() {
+  document.getElementById("6").innerHTML = "because she is";
+}
+
+function word6() {
+  document.getElementById("6").innerHTML = "Adorable";
+}
